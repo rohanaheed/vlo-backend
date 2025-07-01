a8cd9461b6c0ba171c788fde9e780b5ec097b139
Rukhsar Irshad <chrukhsar631@gmail.com>
2025-07-01 03:48:50 -0400

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,12 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["*"]
+  })
+);
 
 // Swagger setup
 const swaggerOptions = {
