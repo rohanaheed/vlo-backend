ea61b7eec881ee536b5d988db51c8a0c743a2251
ibrahim-453 <ranaibrahim453@gmail.com>
2025-12-22 22:52:28 +0500

    deleted prevoius customer addon and created a new customer package table with same fields

---
+++ src/entity/CustomerPackage.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class CustomerPackage {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ default: 0 })
+  customerId!: number;
+
+  @Column({ default: 0 })
+  packageId!: number;
+
+  @Column("json", { nullable: true })
+  addOns! : { module?: string, feature?: string, monthlyPrice?: number, yearlyPrice?: number, discount?: number, description?: string }[];
+
+  @Column({ default: false })
+  isDelete!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
